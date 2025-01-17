935c2ec4a6ad1779acdd73fd7ba1efe5171dfa88
ak.ahmadov <ak.ahmadov@newuu.uz>
2025-01-17 12:24:51 +0500

    My first commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int age;
+    cout << "Hello, My faculty is Economics & Data Science" << endl;
+    cout << "Hello, how old are you?" << endl;
+    cin >> age;
+    cout <<"So, your age is: " << age << endl;
+    cout <<"Thank you!" << endl;
+    return 0;
+}
