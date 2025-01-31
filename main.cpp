3c46fdff29fe8899498e651089ec73a4dff02ff6
ak.ahmadov <ak.ahmadov@newuu.uz>
2025-01-31 13:12:33 +0500

    Week3

---
+++ main.cpp
@@ -0,0 +1,148 @@
+#include <iostream>
+using namespace std;
+int main() {
+//Probelem 1
+
+    // int num1, num2, num3, num4, num5,sum;
+    // cout<<"Enter five numbers: ";
+    // cin>>num1>>num2>>num3>>num4>>num5;
+    // sum=num1+num2+num3+num4+num5;
+    // cout<<"Sum is: "<<sum;
+
+//Problem 2
+    // int id,yearofstudy,studyfee;
+    // float gpa;
+    // cout<<"Enter your id: ";
+    // cin>>id;
+    // cout<<"Enter your year of study: ";
+    // cin>>yearofstudy;
+    // cout<<"Enter your gpa: ";
+    // cin>>gpa;
+    // cout<<"Enter your study fee: ";
+    // cin>>studyfee;
+    // cout<<"Your id: "<<id<<endl;
+    // cout<<"Year of study: "<<yearofstudy<<endl;
+    // cout<<"Gpa: "<<gpa<<endl;
+    // cout<<"Study fee: "<<studyfee<<endl;
+
+//Problem 3
+    // int temp_Cel,temp_Fah;
+    // cout<<"Enter temperature in Celsius: ";
+    // cin>>temp_Cel;
+    // temp_Fah=(temp_Cel*9/5)+32;
+    // cout<<"The temperature in Celsius: "<<temp_Cel<<endl;
+    // cout<<"The temperature in Fahrenheit: "<<temp_Fah<<endl;
+
+//Problem 4
+    // double cost_per_item;
+    // int quantity_of_items;
+    // double total_cost;
+    // cout << "Enter cost per item: ";
+    // cin >> cost_per_item;
+    // cout << "Enter quantity of items: ";
+    // cin >> quantity_of_items;
+    // total_cost = cost_per_item * quantity_of_items;
+    // cout<<"Total Cost: "<<total_cost<<endl;
+
+//Problem 5
+    // int age_father, age_mother,age_sister, age_myself, avg_age;
+    // cout<<"Enter your age: ";
+    // cin>>age_father;
+    // cout<<"Enter your age: ";
+    // cin>>age_mother;
+    // cout<<"Enter your age: ";
+    // cin>>age_sister;
+    // cout<<"Enter your age: ";
+    // cin>>age_myself;
+    // avg_age = (age_father + age_mother + age_sister +age_myself) / 4;
+    // cout<<"Average age is: "<<avg_age<<endl;
+
+//Problem 6
+    // int length_rec, width_rec, per_rec, area_rec;
+    // cin >> length_rec >> width_rec;
+    // area_rec = length_rec * width_rec;
+    // per_rec = (length_rec + width_rec) * 2;
+    // cout <<"The area of the rectangle is " <<area_rec << endl;
+    // cout <<"The perimeter of the rectangle is " <<per_rec << endl;
+
+//Problem 7
+    // int divid, divis, quotient, rem;
+    // cin >> divid >> divis;
+    // quotient = divid / divis;
+    // rem = divid - (divis * quotient);
+    // cout <<"The quotient of the division is : "<< quotient << endl;
+    // cout <<"The remainder of the division is : " << rem << endl;
+
+//Problem 8
+    // int usd, exchange_rate, equiv_amount;
+    // cin >> usd >> exchange_rate;
+    // equiv_amount = usd * exchange_rate;
+    // cout <<"The equivalent amount in UZS is: " << equiv_amount << endl;
+
+//Problem 9
+    // int radius;
+    // float area;
+    // cout << "Enter the radius of the circle: ";
+    // cin >> radius;
+    // area = M_PI * radius * radius;
+    // cout << "Surface area of circle with r is: " << area << endl;
+
+//Problem 10
+    // int radius, height;
+    // float volume;
+    // cout << "Enter radius: ";
+    // cin >> radius;
+    // cout << "Enter height: ";
+    // cin >> height;
+    // volume = 3.14 * radius * radius * height;
+    // cout << "The volume is: " << volume << endl;
+
+//Problem 11
+    // int distance, time, velocity_km;
+    // float velocity_mph;
+    // cout << "Enter distance travelled in km: ";
+    // cin >> distance;
+    // cout << "Enter time travelled in km: ";
+    // cin >> time;
+    // velocity_km = distance / time;
+    // velocity_mph = velocity_km / 1.609f;
+    // cout << "Velocity in km: " << velocity_km << endl;
+    // cout << "Velocity in mph: " << velocity_mph << endl;
+
+ //Problem 12
+    // int number_of_minutes, hours, remaining_minutes;
+    // cin >> number_of_minutes;
+    // hours = number_of_minutes / 60;
+    // remaining_minutes = number_of_minutes % 60;
+    // cout <<"Total time: " << hours << " hours " << remaining_minutes<< " minutes" << endl;
+
+//Problem 13
+    // int selling_price, variable_cost_per_unit, break_even_point;
+    // int fixed_cost = 10000;
+    // cout << "Please enter the selling price: ";
+    // cin >> selling_price;
+    // cout << "Please enter the variable cost: ";
+    // cin >> variable_cost_per_unit;
+    // break_even_point = fixed_cost / (selling_price - variable_cost_per_unit);
+    // cout << "Selling price: " << selling_price << endl;
+    // cout << "Variable cost: " << variable_cost_per_unit << endl;
+    // cout << "Break even point: " << break_even_point << endl;
+
+//Problem 14
+    // int salary, tax_amount, net_salary;
+    // cin >> salary;
+    // tax_amount = salary * 0.12;
+    // net_salary = salary - tax_amount;
+    // cout <<"Net salary is "<< net_salary << endl;
+
+//Problem 15
+    int orig_price, discount_rate, discount_amount, final_price;
+    cin >> orig_price >> discount_rate;
+    discount_amount = orig_price / 100 * discount_rate; ;
+    final_price = orig_price - discount_amount;
+    cout <<"Final price: " << final_price << endl;
+
+    return 0;
+
+
+}
